6e7e50326a255b5f021bb199a003273feff3d9c0
easyhard <liuzhiyou.cs@gmail.com>
2014-07-31 17:24:43 +0800

    remove qunit, use mocha instead

--- web/public/js/config_requirejs.js
+++ web/public/js/config_requirejs.js
@@ -9,7 +9,6 @@ var require = {
         ace: '/extra/ace-builds/src',
         underscore: '/bower_components/underscore/underscore',
         async: '/bower_components/async/lib/async',
-        qunit: '/bower_components/qunit/qunit/qunit',
         mocha: '/bower_components/mocha/mocha',
         chai: '/bower_components/chai/chai'
     },
@@ -17,13 +16,6 @@ var require = {
     // of course you could also just include them in
     // html. I don't have any bias. Make your judgement!
     shim: {
-        qunit: {
-            exports: 'QUnit',
-            init: function () {
-                QUnit.config.autoload = false;
-                QUnit.config.autostart = false;
-            }
-        },
         mocha: {
             exports: 'mocha',
             init: function () {
--- web/public/test/example.js
+++
@@ -1,8 +0,0 @@
-/*global require */
-require(['qunit'], function (QUnit) {
-    QUnit.test( "hello test", function( assert ) {
-        assert.ok( 1 == "1", "Passed!" );
-    });
-    QUnit.load();
-    QUnit.start();
-});
