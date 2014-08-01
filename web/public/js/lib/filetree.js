b30dbb6508a432b5dfb9137a91261e63b37bd7af
pkuwzr <wzr.cs09@qq.com>
2014-08-01 16:41:32 +0800

    Reconstruct the code, bind file system tree's event and editor' handler in main.js

--- web/public/js/lib/filetree.js
+++ web/public/js/lib/filetree.js
@@ -1,15 +1,16 @@
 define(['jquery', 'fancytree', 'underscore'], function($, fancytree, _) {
     "use strict";
-    function FileTree(id, editor) {
-        this.initFancytree(id, editor);
+    function FileTree(id) {
+        this.fancytree = $(id);
+        this.initFancytree(id);
         return this;
     }
 
     _.extend(FileTree.prototype, {
-        initFancytree: function(id, editor) {
-            $(id).fancytree({
+        initFancytree: function(id) {
+            this.fancytree.fancytree({
                 source: [
-                    {title: 'test', key: '1', folder: true, lazy: true, path: 'test'}
+                    {title: 'root', key: '1', folder: true, lazy: true, path: '.'}
                 ],
                 lazyLoad: function(event, data) {
                     var node = data.node;
@@ -18,14 +19,12 @@ define(['jquery', 'fancytree', 'underscore'], function($, fancytree, _) {
                         data: {path: node.data.path}
                     };
                 },
-                checkbox: false,
-                click: function(event, data) {
-                    var node = data.node;
-                    if (!node.folder) {
-                        editor.jumpTo(node.data.path);
-                    }
-                }
+                checkbox: false
             });
+        },
+
+        bind: function(event, callback) {
+            this.fancytree.bind(event, callback);
         }
     });
 
--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,7 +1,14 @@
 /**
  * This is a main entry point of all javascript files.
  */
-require(['editor', 'filetree'], function (Editor, FileTree) {
+require(['editor', 'filetree', 'jquery', 'jquery-ui', 'fancytree'], function (Editor, FileTree, $, ui, ft) {
     var editor = new Editor('code');
-    var filetree = new FileTree('#fstree', editor);
+    var filetree = new FileTree('#fstree');
+    filetree.bind('click', function(event) {
+        var node = $.ui.fancytree.getNode(event);
+        if (!node.folder) {
+            editor.jumpTo({path: node.data.path});
+        }
+    });
+
 });
